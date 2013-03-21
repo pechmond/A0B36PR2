c3cb44728548e3e56bfd8ab26eb511c20c78cfe1
Ondra <Ondra@Notebook>
2013-03-21 23:06:34 +0100

    Prvni commit :)

--- Lode/src/lode/Lode.java
+++ Lode/src/lode/Lode.java
@@ -14,6 +14,6 @@ public class Lode {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
 }
